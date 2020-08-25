2e8065fa9e8f117a328b971c75cb704d59c5543f
Flavius890 <65131626+Flavius890@users.noreply.github.com>
2020-08-25 15:40:48 +0300

    Text is translated by the api.

--- models/m_translations.php
+++ models/m_translations.php
@@ -1,8 +1,10 @@
 <?php
 
-class Translations{
+use function PHPSTORM_META\type;
 
-    private $apostrof = "'";
+include('resources/api_credentials.php');
+
+class Translations{
 
     function __construct()
     {
@@ -11,7 +13,27 @@ class Translations{
 
     public function translate($textToTranslate){
 
-        return $textToTranslate." Translated";
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, apiURL.'/v3/translate?version=2018-05-01');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"text\":[\"".$textToTranslate."\"],\"model_id\":\"en-es\"}");
+        curl_setopt($ch, CURLOPT_USERPWD, 'apikey' . ':' . apiKey);
+
+        $headers = array();
+        $headers[] = 'Content-Type: application/json';
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $result_json= curl_exec($ch);
+        $error='No Error';
+        if (curl_errno($ch)) {
+            $error = curl_error($ch);
+        }
+        curl_close($ch);
+        $translation=json_decode($result_json, true);
+
+        return $textToTranslate." Translated, Result: ".$result_json." Errors: ".$error;
     }
 }
 
---
+++ resources/api_credentials.php
@@ -0,0 +1,7 @@
+<?php
+
+define("apiKey", "BPGT3gaA_9_Mu7BmySQH4A9SYKyRA38-jinBbZ8H2uIP");
+define("apiURL", "https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/5b86d499-079d-4f3e-a508-ac78702d62a6");
+
+
+?>
